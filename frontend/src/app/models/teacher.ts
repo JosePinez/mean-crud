476d178667c4cba8d3650a71a24b1d32abd92218
JosePiñez <josemanuelpinez.17@campuscamara.es>
2020-01-22 20:50:52 +0100

    COMMIT - Create services and component

--- frontend/src/app/models/teacher.ts
+++ frontend/src/app/models/teacher.ts
@@ -1,5 +1,11 @@
 export class Teacher {
 
+    _id: String;
+    name: String;
+    surname: String;
+    area: String;
+    salary: Number;
+
     constructor(_id='', name='', surname='', area='', salary=0){
         this._id =_id;
         this.name = name;
@@ -7,10 +13,5 @@ export class Teacher {
         this.area = area;
         this.salary = salary;
     }
-
-    _id: string;
-    name: string;
-    surname: string;
-    area: string;
-    salary: number;
 }
+
