f9765ac7c661dd3f0d4bab5dfcfeb6cb0ba65a7d
JosePiñez <josemanuelpinez.17@campuscamara.es>
2020-02-10 19:48:38 +0100

    COMMIT - Finished register

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,6 +2,9 @@ import { Component } from '@angular/core';
 import { User } from './models/user';
 import { UserService } from './services/users.service';
 import { NgForm } from '@angular/forms';
+import { TeachersService } from './services/teachers.service';
+import { Router } from '@angular/router';
+
 
 declare var M: any;
 
@@ -14,17 +17,18 @@ declare var M: any;
 export class AppComponent {
   public title = 'Teachers app';
   public user: User;
+  public user_register: User;
   public identity = null;
   public token = null;
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private router: Router) {
     this.user = new User('', '', '', '', '', 'ROLE_USER', '');
+    this.user_register = new User('', '', '', '', '', 'ROLE_USER', '');
   }
   ngOnInit() {
+
     this.identity = this.userService.getIdentity();
     this.token = this.userService.getToken();
-    console.log(this.identity);
-    console.log(this.token);
   }
 
   public onSubmitLogin(form: NgForm) {
@@ -34,44 +38,40 @@ export class AppComponent {
       password: form.value.password,
       getHash: true
     };
-    if (form.value.getHash) {
-    } else {
-      // Get data user
-      this.userService.signUp(form.value)
-        .subscribe(
-          response => {
-            const identity = response['user'];
-            this.identity = identity;
-            if (this.token != null) {
-              M.toast({ html: 'Ok Login' });
-            } else {
-              // Create element token
-              localStorage.setItem('identity', JSON.stringify(identity));
-              // Get Token
-              this.userService.signUp(params)
-                .subscribe(res => {
-                  const token = res['token'];
-                  this.token = token;
-                  if (this.token.length <= 0) {
-                    M.toast({ html: 'Error Login' });
-                  } else {
-                    localStorage.setItem('token', token);
-                  }
-                }, err => {
-                  M.toast({ html: 'Not Login' });
+    // Get data user
+    this.userService.signUp(form.value)
+      .subscribe(
+        response => {
+          const identity = response['user'];
+          this.identity = identity;
+          if (this.token != null) {
+            M.toast({ html: 'Ok Login' });
+          } else {
+            // Create element token
+            localStorage.setItem('identity', JSON.stringify(identity));
+            // Get Token
+            this.userService.signUp(params)
+              .subscribe(res => {
+                const token = res['token'];
+                this.token = token;
+                if (this.token.length <= 0) {
+                  M.toast({ html: 'Error Login' });
+                } else {
+                  localStorage.setItem('token', token);
+                  this.user = new User('', '', '', '', '', 'ROLE_USER', '');
                 }
-                );
-              // Persist in LocalStorage
-
-            }
-            M.toast({ html: 'Login successfully' });
-          },
-          error => {
-            M.toast({ html: 'Not Login' });
+              }, err => {
+                M.toast({ html: 'Not Login' });
+              }
+              );
+            // Persist in LocalStorage
           }
-        );
-    }
-
+          M.toast({ html: 'Login successfully' });
+        },
+        error => {
+          M.toast({ html: 'Not Login' });
+        }
+      );
   }
   logOut() {
     localStorage.removeItem('identity');
@@ -80,6 +80,32 @@ export class AppComponent {
     this.identity = null;
     this.token = null;
     M.toast({ html: 'Logout successfully, thanks for you visit' });
+    this.router.navigate(['/']);
   }
 
+  onSubmitRegister() {
+    const params = {
+      name: this.user_register.name,
+      surname: this.user_register.surname,
+      email: this.user_register.email,
+      password: this.user_register.password,
+      role: 'ROLE_USER',
+      image: ''
+    };
+
+    this.userService.register(params)
+      .subscribe(
+        response => {
+          const user = response['user'];
+          this.user_register = user;
+          if (!user._id) {
+            M.toast({ html: 'Not register' });
+          } else {
+            M.toast({ html: 'User register successfully. You can login use: ' + this.user_register.email });
+            this.user_register = new User('', '', '', '', '', 'ROLE_USER', '');
+          }
+        },
+        error => { M.toast({ html: 'Not register' }); }
+      );
+  }
 }
--- frontend/src/app/services/teachers.service.ts
+++ frontend/src/app/services/teachers.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Teacher } from '../models/teacher';
+
 
 @Injectable({
   providedIn: 'root'
@@ -10,21 +11,27 @@ export class TeachersService {
   selectedTeacher: Teacher;
   teachers: Teacher[];
 
-  constructor(private http: HttpClient){
-      this.selectedTeacher = new Teacher();
-   }
-
-  getTeachers(){
-    return this.http.get(this.URL_API);
+  constructor(private http: HttpClient) {
+    this.selectedTeacher = new Teacher();
   }
 
-  createTeacher(teacher: Teacher){
+  getTeachers() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Authorization': localStorage.getItem('token')
+      })
+    };
+    return this.http.get(this.URL_API, httpOptions);
+  }
+
+  createTeacher(teacher: Teacher) {
     return this.http.post(this.URL_API, teacher);
   }
-  editTeacher(teacher: Teacher){
+  editTeacher(teacher: Teacher) {
     return this.http.put(this.URL_API + `/${teacher._id}`, teacher);
   }
-  deleteTeacher(_id: String ){
+  deleteTeacher(_id: String) {
     return this.http.delete(this.URL_API + `/${_id}`);
   }
 }
